58522bad4a2040e962085b75f08b0a16be3ea918
widhofaisal <widhofaisal26@gmail.com>
2023-04-07 08:41:37 +0700

    add jwt aut

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,10 +1,11 @@
 package controller
 
 import (
-	"learn/mvc/model"
-	"learn/mvc/config"
-	"net/http"
 	"github.com/labstack/echo/v4"
+	"learn/mvc/config"
+	"learn/mvc/middleware"
+	"learn/mvc/model"
+	"net/http"
 )
 
 func GetUsersController(c echo.Context) error {
@@ -37,5 +38,34 @@ func CreateUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"message": "success create user",
 		"user":    user,
+	})
+}
+
+func LoginUserController(c echo.Context) error {
+	// binding data
+	user := model.User{}
+	c.Bind(&user)
+
+	err := config.DB.Where("email=? AND password=?", user.Email, user.Password).First(&user).Error
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "failed login",
+			"error":   err.Error(),
+		})
+	}
+
+	token, err := middleware.CreateToken(user.ID, user.Name)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "failed login",
+			"error":   err.Error(),
+		})
+	}
+
+	userResponse := model.UserResponse{ID: user.ID, Name: user.Name, Email: user.Email, Token: token}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create user",
+		"user":    userResponse,
 	})
 }
--- route/route.go
+++ route/route.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	"learn/mvc/constant"
 	"learn/mvc/controller"
 	m "learn/mvc/middleware"
 
@@ -13,14 +14,20 @@ func New() *echo.Echo {
 
 	e.GET("/users", controller.GetUsersController)
 	e.POST("/users", controller.CreateUser)
+	e.POST("/login", controller.LoginUserController)
 
 	// Logger
 	m.LogMiddleware(e)
 
 	// Auth Basic
-	eAuthBasic := e.Group("/auth")
+	eAuthBasic := e.Group("/auth")							// grouping just for example using middleware
 	eAuthBasic.Use(mid.BasicAuth(m.BasicAuthDB))
 	eAuthBasic.GET("/users", controller.GetUsersController)
+	
+	// Auth JWT
+	eJwt := e.Group("/jwt")									// grouping just for example using middleware
+	eJwt.Use(mid.JWT([]byte(constant.SECRET_JWT)))
+	eJwt.GET("/users", controller.GetUsersController)
 
 	return e
 }
